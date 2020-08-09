15953af84245d801a439a9b8f8b5145a21d6baef
desirekaleba <desirekaleba@gmail.com>
2020-08-09 23:33:51 +0300

    feat: Add characters component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Characters from './components/Characters/Characters';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+      setItems(data.data);
+      setLoading(false);
+    };
+    fetchData();
+
+  }, []);
+
   return (
     <div className="App">
       <Header/>
+      <Characters isLoading={loading} characters={items} />
     </div>
   );
 }
