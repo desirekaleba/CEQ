23b0d6ac52b33f087069ebc47641d893df869de1
Desire Kaleba <desirekaleba@gmail.com>
2021-02-07 16:54:15 +0300

    Remove blank space and center

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 import Character from '../Character/Character';
+import Loading from '../Common/Loading';
 import './Characters.css';
 
-const Characters = ( { characters, isLoading }) => {
-    return isLoading? <div>Loading...</div> : <div className='wrapper cards'>
-        {
-            characters.map(character => (
-                <Character key={character.char_id} singleCharacter={character}></Character>
-            ))
-        }
-    </div>  
-    
+const Characters = ({ characters, isLoading }) => {
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <div className='cards'>
+      {characters.map((character) => (
+        <Character
+          key={character.char_id}
+          singleCharacter={character}
+        ></Character>
+      ))}
+    </div>
+  );
 };
 
 export default Characters;
